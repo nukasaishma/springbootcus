cea888ea3ce2373d8f54187649ed3bfe00a0e859
nuka <saishma.nuka@dxc.com>
2022-12-17 17:35:12 +0530

    commit

---
+++ src/main/java/com/taskfirst/SpringBootPracticeTask1Application.java
@@ -0,0 +1,14 @@
+package com.taskfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SpringBootPracticeTask1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeTask1Application.class, args);
+	}
+
+}
